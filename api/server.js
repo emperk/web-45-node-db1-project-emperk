d35e84df233622e6c6a10bf53aef514ddb63ffa0
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-09-06 19:11:12 -0700

    t

--- api/server.js
+++ api/server.js
@@ -4,4 +4,10 @@ const server = express();
 
 server.use(express.json());
 
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: '404 Error. Nothing found.',
+  })
+})
+
 module.exports = server;
