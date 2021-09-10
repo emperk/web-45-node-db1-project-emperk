e22f3b9bb65f5ad15441f85b64b20a0783cf6402
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-09-09 20:18:56 -0700

    test

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,17 @@
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR 
+  console.log('checkAccountPayload middleware')
+  next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR 
+  console.log('checkAccountNameUnique middleware')
+  next()
 }
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR 
+  console.log('checkAccountId middleware')
+  next()
 }
